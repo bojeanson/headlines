7ff233ac4f1f21b6a7a906f2a277a8130fdc68c1
Baptiste O'Jeanson <baptiste.ojeanson@insa-rouen.fr>
2017-08-20 14:24:03 +0000

    handling utf8 with jinja

--- headlines.py
+++ headlines.py
@@ -16,11 +16,8 @@ def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     title = first_article.get("title")
-    title = title.encode('utf-8') if title else ''
     published = first_article.get("published")
-    published = published.encode('utf-8') if published else ''
     summary = first_article.get("summary")
-    summary = summary.encode('utf-8') if summary else ''
     return render_template("home.html", title=title, published=published, summary=summary)
 
 
